ab45b63c1b5fabf8a9ef92a885d4de33c324d2f0
huiter rodrigues <61643960+huiterjrv@users.noreply.github.com>
2020-08-08 14:36:43 +0200

    insercion de props manual

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Card from './components/Card'
+import 'bootstrap/dist/css/bootstrap.css'
 
 const container = document.querySelector('div#root')
 
@@ -8,4 +9,13 @@ const container = document.querySelector('div#root')
 
 //resibe dos parametros que elemento renderisara y el segundo donde lo va hacer
 //ReactDOM.render(__Que__,__Donde__)
-ReactDOM.render(<Card/>,container)
+ReactDOM.render(<Card
+          title="Technique Guides"
+          description="Learn amazing street workout and calisthenics"
+          img=""
+          leftColor="#A74CF2"
+          reghtColor="#617BFB"
+                />,container)
+  //esto se le asigno las propiedades al contenido 
+  // es para cargar dinamica mente
+  
