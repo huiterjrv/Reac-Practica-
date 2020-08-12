4f0518c92de71efe903df30ac7acda874f459882
huiter rodrigues <61643960+huiterjrv@users.noreply.github.com>
2020-08-12 11:58:06 +0200

    un componente corto y rapido de usar

---
+++ src/components/Welcome.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './styles/Welcome.css'
+//esto es para declarar de forma rapida un componente, lo malo es que no puedes declarar las stados y su siclo de vida 
+//function Welcome(props){} es lo mismo 
+const Welcome = (props) => {
+    return (
+        <div className="container">
+            <div className="Fitness-User-Info">
+                <h1>Hello {props.name}</h1>
+                <p>Let´s workout to get someone gains!</p>
+            </div>
+        </div>    
+    )
+}
+// se usa en el caso que solo lo quieres renderizar sin modificar 
+
+export default Welcome
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Card from './components/Card'
+import Welcome from './components/Welcome'
 import 'bootstrap/dist/css/bootstrap.css'
 
 const container = document.querySelector('div#root')
@@ -9,13 +10,16 @@ const container = document.querySelector('div#root')
 
 //resibe dos parametros que elemento renderisara y el segundo donde lo va hacer
 //ReactDOM.render(__Que__,__Donde__)
-ReactDOM.render(<Card
+ReactDOM.render(<div>
+      <Welcome name="huiter"/>
+      <Card
           title="Technique Guides"
           description="Learn amazing street workout and calisthenics"
           img=""
           leftColor="#A74CF2"
           reghtColor="#617BFB"
-                />,container)
+                />
+      </div>,container)
   //esto se le asigno las propiedades al contenido 
   // es para cargar dinamica mente
   
